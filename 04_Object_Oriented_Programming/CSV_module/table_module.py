6bdd901e6235a5068d2fd6c3e6eb06d474277e32
Swayam <swayam.goel2013@gmail.com>
2026-09-16 23:12:46 +0530

    updated code from previous class

--- 04_Object_Oriented_Programming/CSV_module/table_module.py
+++ 04_Object_Oriented_Programming/CSV_module/table_module.py
@@ -1,106 +1,64 @@
 class Table:
-    def __init__(self, data):
+    def __init__(self, filePath):
+        file = open(filePath, "r")
+        self.columns = file.readline().strip().split(",")
+        self.col_count = len(self.columns)
 
-        # if data is a string, it is a file path
-        if(isinstance(data, str)):
-            # open the file for reading
-            filePath = data
-            file = open(filePath, "r")
-            # read the first line to get the column names
-            self.columns = file.readline().strip().split(",")
-            self.col_count = len(self.columns)
-            # create an empty table with the column names
-            self.data = {}
-            for col in self.columns:
-                self.data[col] = []
-            # read the rest of the file to get the data rows
-            self.row_count = 0
-            for line in file:
-                values = line.strip().split(",")
-                self.row_count += 1
-                for (i,header) in enumerate(self.columns):
-                    self.data[header].append(values[i])
-            # close the file
-            file.close()
+        self.table = {}
+        for col in self.columns:
+            self.table[col] = []
 
-        # if data is a dict, it is a table
-        if(isinstance(data, dict)):
-            # store the data in the table
-            self.data = data
-            # store metadata
-            self.columns = list(data.keys())
-            self.col_count = len(self.columns)
-            self.row_count = len(data[self.columns[0]])
+        self.row_count = 0
+        for line in file:
+            values = line.strip().split(",")
+            self.row_count += 1
+            for (i,header) in enumerate(self.columns):
+                self.table[header].append(values[i])
+        file.close()
         
     def show(self, filePath = "output.txt"):
-        # calculate the width of each column
         widths = {}
-        for col in self.data:
+        for col in self.table:
             widths[col] = len(col)
             for i in range(self.row_count):
-                widths[col] = max(widths[col], len(self.data[col][i]))
-        # initialize horizontal line
+                widths[col] = max(widths[col], len(self.table[col][i]))
         horizontal = "+"
-        for col in self.data:
+        for col in self.table:
             horizontal += "-" * (widths[col] + 2) + "+"
-        horizontal += "\n"
-        # write the table in the file
+        horizontal += "\n" 
         file = open(filePath, "w")
         file.write(horizontal)
-        for col in self.data:
+        for col in self.table:
             file.write(f"| {col:{widths[col]}} ")
         file.write(f"|\n{horizontal}")
         for i in range(self.row_count):
-            for col in self.data:
-                if(self.data[col][i].isnumeric()):
-                    file.write(f"| {self.data[col][i]:>{widths[col]}} ")
+            for col in self.table:
+                if(self.table[col][i].isnumeric()):
+                    file.write(f"| {self.table[col][i]:>{widths[col]}} ")
                 else:
-                    file.write(f"| {self.data[col][i]:<{widths[col]}} ")
+                    file.write(f"| {self.table[col][i]:<{widths[col]}} ")
             file.write("|\n")
         file.write(horizontal)
         file.close()
 
     def get_size(self):
-        # return the size of the table
         return (self.row_count, self.col_count)
 
     def get_rows(self, row_numbers):
-        # create a new table
-        data = {}
-        for col in self.columns:
-            data[col] = []
-        # filter the specified rows
-        for col in self.columns:
-            for i in range(self.row_count):
-                if(i in row_numbers):
-                    data[col].append(self.data[col][i])
-        # return a new table with the specified rows
-        return Table(data)
-    
+        return Table({col: [self.table[col][i] for i in row_numbers] for col in self.columns})
+
     def get_columns(self, column_names):
-        # create a new table
-        data = {}
-        # filter the specified columns
-        for col in self.columns :
-            if(col in column_names):
-                data[col] = self.data[col]
-        # return a new table with the specified columns
-        return Table(data)
+        return Table({col: self.table[col] for col in column_names})
 
     def get_value(self, row_number, col_name):
-        pass
+        return self.table[col_name][row_number]
 
 if __name__ == "__main__":
-    # read the table data from the file
     pokedex = Table("dataset.csv")
+    # print("Table size = ", pokedex.get_size())
+    # pokedex.show()
 
-    # print the table
     pokedex.show()
+    stats = pokedex.get_columns(["species_ID", "HP", "phAtk", "phDef"])
+    print("Stats size = ", stats.get_size())
 
-    # get selected rows from the table
-    few_rows = pokedex.get_rows([0, 1, 2])
-    few_rows.show("few_rows.txt")
-
-    # get selected columns from the table
-    few_cols = pokedex.get_columns(["name", "type_1", "type_2"])
-    few_cols.show("few_cols.txt")
